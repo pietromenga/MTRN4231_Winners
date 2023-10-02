4c327076bfa2284018c7b658ca5dcf778ffebb32
andrew <smokingpacman@gmail.com>
2023-10-02 18:55:25 +1100

    renamed nodes fixed typo

--- winners_pkg/src/camTest.cpp
+++ winners_pkg/src/camTest.cpp
@@ -31,7 +31,7 @@ class MinimalPublisher : public rclcpp::Node
 {
 public:
     MinimalPublisher()
-    : Node("MinalPublisher"), count_(0)
+    : Node("MinimalPublisher"), count_(0)
     {
         publisher_ = this->create_publisher<geometry_msgs::msg::Pose>("topic", 10);
         timer_ = this->create_wall_timer(
