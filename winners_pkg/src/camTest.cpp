13fa8395059730bff1546cf25be36277550e8da2
andrew <smokingpacman@gmail.com>
2023-10-03 11:14:21 +1100

    This at least compiles now but still no work

--- winners_pkg/src/camTest.cpp
+++ winners_pkg/src/camTest.cpp
@@ -1,102 +1,54 @@
-#include <linux/videodev2.h>
+#include <chrono>
 #include <fcntl.h>
-#include <unistd.h>
+#include <linux/videodev2.h>
+#include <memory>
+#include <stdio.h>
+#include <string.h>
 #include <sys/ioctl.h>
-#include <sys/mman.h>
+#include <unistd.h>
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
 
+using namespace std::chrono_literals;
+
 class ImagePublisher : public rclcpp::Node
 {
 public:
-    ImagePublisher() : Node("imagePublisher")
+    ImagePublisher() : Node("image_publisher")
     {
         publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/image_raw", 10);
-
-        // Initialize V4L2
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(16), std::bind(&ImagePublisher::captureAndPublish, this));
         init_v4l2();
-
-        timer_ = this->create_wall_timer(
-            16ms, std::bind(&ImagePublisher::captureAndPublish, this));
     }
 
 private:
     void init_v4l2()
     {
-        fd = open("/dev/video0", O_RDWR);
-        if (fd == -1) {
-            perror("Opening video device");
-            return;
-        }
-
-        // TODO: Add more settings here like frame rate, resolution, etc.
-
-        // Initialize buffer
-        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        buf.memory = V4L2_MEMORY_MMAP;
-        // TODO: Fill other buf fields as necessary
-
-        // Memory mapping
-        buffer = mmap(NULL, 
-                      /* length of the mapped area */,
-                      PROT_READ | PROT_WRITE,
-                      MAP_SHARED,
-                      fd, 
-                      buf.m.offset);
-
-        if (buffer == MAP_FAILED) {
-            perror("Memory mapping failed");
-            return;
-        }
-
-        // Queue the buffer
-        if (ioctl(fd, VIDIOC_QBUF, &buf) == -1) {
-            perror("Queue buffer");
-            return;
-        }
-
-        // Start capturing
-        enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        if (ioctl(fd, VIDIOC_STREAMON, &type) == -1) {
-            perror("Start capture");
-            return;
-        }
+        // Initialization code for v4l2
+        // Omitted for brevity, use ioctl calls to set up your camera
     }
 
     void captureAndPublish()
     {
-        if (ioctl(fd, VIDIOC_DQBUF, &buf) == -1) {
-            perror("Retrieve frame");
-            return;
-        }
-
-        // Convert this to ROS2 sensor_msgs::msg::Image and publish.
-        sensor_msgs::msg::Image image_msg;
+        auto image_msg = sensor_msgs::msg::Image();
         image_msg.header.stamp = this->now();
-        image_msg.height = /* height */;
-        image_msg.width = /* width */;
-        image_msg.encoding = "bgr8";  
-        image_msg.step = /* step */;
-        image_msg.data = std::vector<uint8_t>((uint8_t*)buffer, (uint8_t*)buffer + buf.length);
+        image_msg.height = 1080;
+        image_msg.width = 1920;
+        image_msg.encoding = "bgr8"; // or "rgb8" or whatever your camera produces
+        image_msg.step = 1920 * 3;   // step = width * byte_per_pixel for color images
+
+        // Capture code here to fill image_msg.data
+        // Omitted for brevity
 
         publisher_->publish(image_msg);
-
-        // Requeue buffer
-        if (ioctl(fd, VIDIOC_QBUF, &buf) == -1) {
-            perror("Queue buffer");
-            return;
-        }
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
-    int fd;
-    void *buffer;
-    struct v4l2_buffer buf;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<ImagePublisher>());
