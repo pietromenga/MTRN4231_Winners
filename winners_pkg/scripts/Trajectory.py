7a429f57d6b7cb15a936937c2a9c132862d07c6b
Andrew <smokingpacman@gmail.com>
2023-10-28 18:13:18 +1100

    rudimentary tradjectory calculation

--- winners_pkg/scripts/Trajectory.py
+++ winners_pkg/scripts/Trajectory.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
 import rclpy
-import numpy as np
 from rclpy.node import Node
 import time
-from builtin_interfaces.msg import Time 
-
 from geometry_msgs.msg import PoseStamped
 from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
-
 
 class TrajectoryCalculator(Node):
 
@@ -26,84 +22,45 @@ class TrajectoryCalculator(Node):
         self.dt = 0.1
         self.timer = self.create_timer(self.dt, self.timer_callback)
 
-        self.itemCount = 0
-        self.posX = []
-        self.posY = []
-        self.posZ = []
-        self.timeList = []
-        self.startTime = time.time()
-        self.to_frame_rel = 'base_link'
-
-        self.minY, self.maxY = 0, 0.4
-        self.minX, self.maxX = -0.9, -0.5
-        self.minZ, self.maxZ = 0, 0.25
+        self.last_x = self.last_y = self.last_z = None
+        self.vx = self.vy = self.vz = 0
 
     def timer_callback(self):
 
         # Get position of ball in base frame
         try:
             tBall = self.tf_buffer.lookup_transform(
-                self.to_frame_rel,
+                'base_link',
                 'ball_tf',
                 rclpy.time.Time())
-            
-            tEndEff = self.tf_buffer.lookup_transform(
-                self.to_frame_rel,
-                'tool0',
-                rclpy.time.Time())
         except TransformException as ex:
-            # self.get_logger().info(
-            #     f'Could not transform {to_frame_rel} to {from_frame_rel}: {ex}')
             return
-        
-        # Store transforms
-        if self.itemCount < 5:
-            self.appendTransform(tBall)
-        else: # Enough to regress
-            currentTime = time.time() - self.startTime
-            self.appendTransform(tBall)
 
-            xCoeff = np.polyfit(self.timeList, self.posX, 2)
-            yCoeff = np.polyfit(self.timeList, self.posY, 2)
-            zCoeff = np.polyfit(self.timeList, self.posZ, 2)
+        # Current position
+        x, y, z = tBall.transform.translation.x, tBall.transform.translation.y, tBall.transform.translation.z
 
-            dt = 0.01
-            timeSteps = np.arange(currentTime, currentTime + 1, dt)
-            minDist = 100
-            ballPredTarget = ()
-            # for ti in reversed(timeSteps): # Reversed as solutions farthest away will be closest
-            #     # xyz at time step ti
-            #     x = float(xCoeff[0] * ti ** 2 + xCoeff[1] * ti + xCoeff[2])
-            #     y = float(yCoeff[0] * ti ** 2 + yCoeff[1] * ti + yCoeff[2])
-            #     z = float(zCoeff[0] * ti ** 2 + zCoeff[1] * ti + zCoeff[2])
+        # If this is the first measurement, just store the position
+        if self.last_x is None:
+            self.last_x, self.last_y, self.last_z = x, y, z
+            return
 
-                # Calc distance to tool
-                # dist2tool = np.sqrt(
-                #     (x - tEndEff.transform.translation.x)**2 + 
-                #     (y - tEndEff.transform.translation.y)**2 +
-                #     (z - tEndEff.transform.translation.z)**2 
-                # )
+        # Compute velocity (assuming constant time step for simplicity)
+        self.vx = (x - self.last_x) / self.dt
+        self.vy = (y - self.last_y) / self.dt
+        self.vz = (z - self.last_z) / self.dt
 
-                # Update mindist
-                # if dist2tool < minDist and self.inCatchingRange(x,y,z):
-                    # minDist = dist2tool 
-                    # ballPredTarget = (x,y,z)
+        # Predict next position
+        predicted_x = x + self.vx * self.dt
+        predicted_y = y + self.vy * self.dt
+        predicted_z = z + self.vz * self.dt - 9.81 * 0.5 * self.dt**2  # accounting for gravity
 
-            secondInFuture = currentTime + 1
-            x = float(xCoeff[0] * secondInFuture ** 2 + xCoeff[1] * secondInFuture + xCoeff[2])
-            y = float(yCoeff[0] * secondInFuture ** 2 + yCoeff[1] * secondInFuture + yCoeff[2])
-            z = float(zCoeff[0] * secondInFuture ** 2 + zCoeff[1] * secondInFuture + zCoeff[2])
-            ballPredTarget = (x,y,z)
-                
+        # Update last position
+        self.last_x, self.last_y, self.last_z = x, y, z
 
-            # send off prediction
-            if ballPredTarget != ():
-                self.sendBallPred(*ballPredTarget) #unpack tuple into args
+        # Send off prediction
+        self.sendBallPred(predicted_x, predicted_y, predicted_z)
 
-            # Remove last point to get new regression
-            self.removeLast()
-
-    def sendBallPred(self, x,y,z):
+    def sendBallPred(self, x, y, z):
         pred = PoseStamped()
         pred.header.stamp = self.get_clock().now().to_msg()
         pred.header.frame_id = "base_link"
@@ -111,26 +68,6 @@ class TrajectoryCalculator(Node):
         pred.pose.position.y = y
         pred.pose.position.z = z
         self.pred_publisher_.publish(pred)
-
-    def inCatchingRange(self, x, y, z):
-        # xRange = x > self.minX and x < self.maxX
-        # yRange = y > self.minY and y < self.maxY
-        # zRange = z > self.minZ and z < self.maxZ    
-        return True
-
-    def removeLast(self):
-        self.posX.clear()
-        self.posY.clear()
-        self.posZ.clear()
-        self.timeList.clear()
-        self.itemCount -= 1
-    
-    def appendTransform(self, t): 
-        self.posX.append(t.transform.translation.x)
-        self.posY.append(t.transform.translation.y)
-        self.posZ.append(t.transform.translation.z)
-        self.timeList.append(time.time() - self.startTime)
-        self.itemCount += 1
 
 def main(args=None):
     rclpy.init(args=args)
