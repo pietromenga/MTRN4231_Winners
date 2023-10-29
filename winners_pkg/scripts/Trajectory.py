1953d3c7583d76cd099b336cd24722aeccb51477
Andrew <smokingpacman@gmail.com>
2023-10-29 22:12:43 +1100

    tradjectory works but barely

--- winners_pkg/scripts/Trajectory.py
+++ winners_pkg/scripts/Trajectory.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
@@ -12,56 +11,42 @@ class TrajectoryCalculator(Node):
     def __init__(self):
         super().__init__('Trajectory_Calculator')
 
-        # Prediction publisher
         self.pred_publisher_ = self.create_publisher(PoseStamped, 'ball_prediction', 10)
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        self.dt = 0.5
+        self.dt = 0.5  # Prediction time
         self.timer = self.create_timer(self.dt, self.timer_callback)
 
-        self.last_x_hat = self.last_y_hat = self.last_z_hat = None
-        self.vx_hat = self.vy_hat = self.vz_hat = 0
-
-        self.alpha = 0.8  # Tunable parameter for position update
-        self.beta = 0.02  # Tunable parameter for velocity update
+        self.last_x = self.last_y = self.last_z = None
+        self.last_time = None
 
     def timer_callback(self):
 
-        # Get position of ball in base frame
         try:
-            tBall = self.tf_buffer.lookup_transform(
-                'base_link',
-                'ball_tf',
-                rclpy.time.Time())
+            tBall = self.tf_buffer.lookup_transform('base_link', 'ball_tf', rclpy.time.Time())
         except TransformException as ex:
             return
 
-        # Current position
+        current_time = tBall.header.stamp.sec + tBall.header.stamp.nanosec * 1e-9  # Convert to seconds
         x, y, z = tBall.transform.translation.x, tBall.transform.translation.y, tBall.transform.translation.z
 
-        # If this is the first measurement, just store the position
-        if self.last_x_hat is None:
-            self.last_x_hat, self.last_y_hat, self.last_z_hat = x, y, z
-            return
+        if self.last_x is not None:
+            elapsed_time = current_time - self.last_time
+            vx = (x - self.last_x) / elapsed_time
+            vy = (y - self.last_y) / elapsed_time
+            vz = (z - self.last_z) / elapsed_time
 
-        # Alpha-Beta filter update
-        self.vx_hat = self.vx_hat + self.beta * ((x - self.last_x_hat) - self.vx_hat * self.dt)
-        self.vy_hat = self.vy_hat + self.beta * ((y - self.last_y_hat) - self.vy_hat * self.dt)
-        self.vz_hat = self.vz_hat + self.beta * ((z - self.last_z_hat) - self.vz_hat * self.dt - 9.81 * 0.5 * self.dt**2)
+            # Predict next position
+            predicted_x = x + vx * self.dt
+            predicted_y = y + vy * self.dt
+            predicted_z = z + vz * self.dt
 
-        self.last_x_hat = self.last_x_hat + self.vx_hat * self.dt + self.alpha * ((x - self.last_x_hat) - self.vx_hat * self.dt)
-        self.last_y_hat = self.last_y_hat + self.vy_hat * self.dt + self.alpha * ((y - self.last_y_hat) - self.vy_hat * self.dt)
-        self.last_z_hat = self.last_z_hat + self.vz_hat * self.dt + self.alpha * ((z - self.last_z_hat) - self.vz_hat * self.dt - 9.81 * 0.5 * self.dt**2)
+            self.sendBallPred(predicted_x, predicted_y, predicted_z)
 
-        # Predict next position
-        predicted_x = self.last_x_hat + self.vx_hat * self.dt
-        predicted_y = self.last_y_hat + self.vy_hat * self.dt
-        predicted_z = self.last_z_hat + self.vz_hat * self.dt - 9.81 * 0.5 * self.dt**2  # accounting for gravity
-
-        # Send off prediction
-        self.sendBallPred(predicted_x, predicted_y, predicted_z)
+        self.last_x, self.last_y, self.last_z = x, y, z
+        self.last_time = current_time
 
     def sendBallPred(self, x, y, z):
         pred = PoseStamped()
