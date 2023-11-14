25613dc0d44150e0ca02d4a910ed1400b58808b3
Young <z5309347@ad.unsw.edu.au>
2023-11-15 03:31:19 +1100

    Created instructions doc to run all nodes, Also new launch file (Has to be tested)

---
+++ RUN.md
@@ -0,0 +1,6 @@
+Step instruction: 
+
+1. ./setupRealur5e 
+2. ros2 launch winners_pkg winners_launch.py
+3. ros2 run CameraCV opencv_test.py
+4. ros2 run util_arduino_serial util_arduino_serial
---
+++ winners_pkg/launch/TestLaunch.py
@@ -0,0 +1,143 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution, Command, FindExecutable
+from launch_ros.substitutions import FindPackageShare
+
+def get_robot_description():
+    joint_limit_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", "ur5e", "joint_limits.yaml"]
+    )
+    kinematics_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", "ur5e", "default_kinematics.yaml"]
+    )
+    physical_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", "ur5e", "physical_parameters.yaml"]
+    )
+    visual_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", "ur5e", "visual_parameters.yaml"]
+    )
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("ur_description"), "urdf", "ur.urdf.xacro"]),
+            " ",
+            "robot_ip:=172.17.0.2",
+            " ",
+            "joint_limit_params:=",
+            joint_limit_params,
+            " ",
+            "kinematics_params:=",
+            kinematics_params,
+            " ",
+            "physical_params:=",
+            physical_params,
+            " ",
+            "visual_params:=",
+            visual_params,
+            " ",
+           "safety_limits:=",
+            "true",
+            " ",
+            "safety_pos_margin:=",
+            "0.15",
+            " ",
+            "safety_k_position:=",
+            "20",
+            " ",
+            "name:=",
+            "ur",
+            " ",
+            "ur_type:=",
+            "ur5e",
+            " ",
+            "prefix:=",
+            '""',
+            " ",
+        ]
+    )
+
+
+    robot_description = {"robot_description": robot_description_content}
+    return robot_description
+
+def get_robot_description_semantic():
+    # MoveIt Configuration
+    robot_description_semantic_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("ur_moveit_config"), "srdf", "ur.srdf.xacro"]),
+            " ",
+            "name:=",
+            # Also ur_type parameter could be used but then the planning group names in yaml
+            # configs has to be updated!
+            "ur",
+            " ",
+            "prefix:=",
+            '""',
+            " ",
+        ]
+    )
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_content
+    }
+    return robot_description_semantic
+
+def generate_launch_description():
+    robot_description = get_robot_description()
+    robot_description_semantic = get_robot_description_semantic()
+
+    return LaunchDescription([
+        Node(
+            package='balldetect',
+            name='ball_detector',
+            executable='ball_detector',
+        ),
+        Node(
+            package='balldetect',
+            name='target_detector',
+            executable='target_detector',
+        ),
+        Node(
+            package='winners_pkg',
+            name='Trajectory',
+            executable='Trajectory.py',
+        ),
+        Node(
+            package='winners_pkg',
+            name='Transforms',
+            executable='Transforms',
+        ),
+        Node(
+            package='winners_pkg',
+            name='Control',
+            executable='Control.py',
+            parameters=[
+                robot_description,
+                robot_description_semantic,
+            ],
+        ),  
+        Node(
+            package='winners_pkg',
+            name='Visualisations',
+            executable='Visualisations',
+        ),
+        Node(
+            package='winners_pkg',
+            name='Brain',
+            executable='Brain',
+        )
+        Node(
+            package='CameraCV_pkg',
+            name='opencv_test',
+            executable='opencv_test',
+        )
+        Node(
+            package='util_arduino_serial',
+            name='util_arduino_serial',
+            executable='util_arduino_serial',
+        )
+    ])
